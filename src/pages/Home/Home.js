07d5defb306f371a0e6cecd6fb102a27af4c47a4
Raphael Pedra <raphaelpedrap@gmail.com>
2021-10-15 10:11:18 -0300

    History

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
+import {useHistory} from "react-router-dom"
 
 function Home() {
-    return <h1>Home</h1>;
+
+    const history = useHistory();
+
+
+    return (
+        <div>
+         <h1>Home</h1>
+         <button 
+            onClick = {() => {
+                history.push("Login") //history.goBack volta pra página anterior
+             }}
+                >Logout</button>
+        </div>
+    );
 }
 
 export default Home;
