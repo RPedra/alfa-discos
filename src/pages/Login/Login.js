5653b4c13f8c838514fc8a784b8d5735ae13860e
Bernardo <bernardomaia@cpejr.com.br>
2021-10-19 10:40:06 -0300

    Login Funcional Aula 17

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import {Form, Button} from "react-bootstrap";
+import {useHistory} from "react-router-dom";
 import "./Login.css";
 import Cabecalho from "../Cabecalho";
 
 
+
 function Login() {
+
+    const [email, setEmail] = useState();
+    const [senha, setSenha] = useState();
+    const history = useHistory();
+    function login(){
+        if(email == "abc@gmail.com" && senha === "123") {
+            alert ("Bem vindo!");
+            history.push("home");
+        }
+        else alert ("Dados incorretos");
+    }
+
     return (
         <div className="base">
             <div className="justify-content-end">
@@ -15,15 +29,15 @@ function Login() {
              <Form>
                 <div className="inputs">
                  <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="e-mail" />
+                    <Form.Control type="email" placeholder="e-mail" onChange={(e) => setEmail(e.target.value)}  />
                  </Form.Group>
                  <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="senha" />
+                    <Form.Control type="password" placeholder="senha" onChange={(e) => setSenha(e.target.value)}/>
                 </Form.Group>
                     <div className="buttons">
-                        <Button variant="danger">Login</Button>
+                        <Button variant="danger" style={{width:'500px', height: '40px' }} onClick={login} >Login</Button>
                         <br/>
-                        <Button variant="outline-danger">Não possui conta? Clique Aqui</Button>{' '}
+                        <Button variant="outline-danger" style={{width:'500px', height: '40px' }}>Não possui conta? Clique Aqui</Button>{' '}
                     </div>
                 </div>
              </Form>
