86eb5f62df31c97183fcae942f0cb2d68f68be67
Bernardo <bernardomaia@cpejr.com.br>
2021-10-22 17:56:38 -0300

    Modal Explorar

--- src/Componentes/cd/cd.js
+++ src/Componentes/cd/cd.js
@@ -3,25 +3,25 @@ import { Card, Button } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import "./cd.css";
 
-function Cd({cd}){
+function Cd({cd, onClick}){
     const history = useHistory();
     return(
-        <div className="cds">
+        <div className="cds" onClick={() => onClick(cd)}>
             <Card style={{ 
-                width: '300px', 
-                height: '300px', 
+                width: '250px', 
+                height: '250px', 
                 top: '217px',
                 margin: '0 auto',  
                 boxShadow: '0px 4px 5px 0px rgba(0,0,0,0.25)',
                 borderRadius: '10px'
                 }}>
-                <Card.Img variant="top" src={cd.imagem} style={{height: '215px', borderRadius: '10px 10px 0px 0px'}} />
+                <Card.Img variant="top" src={cd.imagem} style={{height: '185px', borderRadius: '10px 10px 0px 0px'}} />
                 <Card.Body className="corpo">
+                    <Card.Title className="nome">{cd.nome}</Card.Title>
                     <div className="autoria">
-                    <Card.Title>{cd.nome}</Card.Title>
-                    <Card.Subtitle>{cd.artista}</Card.Subtitle>
-                    </div>
+                    <Card.Subtitle className="artista">{cd.artista}</Card.Subtitle>       
                     <Card.Text className="valor" >R$ {cd.valor}</Card.Text>
+                    </div>
                 </Card.Body>
             </Card>
         </div>
