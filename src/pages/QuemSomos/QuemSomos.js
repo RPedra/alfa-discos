bf630f263b05448ebfdddf200d6862aeae242ef7
Raphael Pedra <raphaelpedrap@gmail.com>
2021-11-03 00:07:32 -0300

    Mudança Quem Somos

--- src/pages/QuemSomos/QuemSomos.js
+++ src/pages/QuemSomos/QuemSomos.js
@@ -3,55 +3,80 @@ import Cabecalho from "../Cabecalho";
 import { Card } from "react-bootstrap";
 import Rodape from "../Rodape";
 import "./QuemSomos.css";
+import Button from "@restart/ui/esm/Button";
+import { useHistory } from "react-router";
 
 function QuemSomos() {
+  const history = useHistory();
+
   return (
     <div className="pagina">
       <Cabecalho />
       <div className="alinhamento">
-      <div
-        className="justify-content-center"
-        style={{
-          backgroundColor: "#A31621",
-          alignItems: "center",
-          margin: "0 auto",
-          maxWidth: "1156px",
-          height: "50px",
-          boxShadow: "-4px 4px 5px 0px rgba(0,0,0,0.15)",
-        }}
-      >
-        <Card.Title rounded style={{ color: "white" }}>
-          Quem Somos
-        </Card.Title>
-      </div>
-      <div className="texto">
-        <h1 style={{ fontSize: "1.5rem" }}>
-          A Alfa Discos foi criada com a finalidade de incentivar e alavancar as
-          vendas e o trabalho de artistas com um baixo orçamento. Divulgamos o
-          trabalho destes artistas e, atraves de nossa plataforma, permitimos
-          então que eles vendam seus álbuns sem a intermediação de uma
-          gravadora. Trabalhamos com todos os tipos de estilos músicais.
-        </h1>
+        <div
+          className="justify-content-center"
+          style={{
+            backgroundColor: "#A31621",
+            alignItems: "center",
+            margin: "0 auto",
+            maxWidth: "1156px",
+            height: "50px",
+            boxShadow: "-4px 4px 5px 0px rgba(0,0,0,0.15)",
+          }}
+        >
+          <Card.Title rounded style={{ color: "white" }}>
+            Quem Somos
+          </Card.Title>
+        </div>
+        <div className="texto">
+          <h1 style={{ fontSize: "1.5rem" }}>
+            A Alfa Discos foi criada com a finalidade de incentivar e alavancar
+            as vendas e o trabalho de artistas com um baixo orçamento.
+            Divulgamos o trabalho destes artistas e, atraves de nossa
+            plataforma, permitimos então que eles vendam seus álbuns sem a
+            intermediação de uma gravadora. Trabalhamos com todos os tipos de
+            estilos músicais.
+          </h1>
 
-        <h1 style={{ fontSize: "1.5rem" }}>
-          Vendemos não apenas CDs, mas o sonho de artistas.
-        </h1>
+          <h1 style={{ fontSize: "1.5rem" }}>
+            Vendemos não apenas CDs, mas o sonho de artistas.
+          </h1>
 
-        <h1 style={{ fontSize: "1.5rem" }}>
-          Oferecemos as mais diversas experiência para nossos clientes, desde a
-          procura pelo CD ideal até a entega do produto em sua residência.
-        </h1>
+          <h1 style={{ fontSize: "1.5rem" }}>
+            Oferecemos as mais diversas experiência para nossos clientes, desde
+            a procura pelo CD ideal até a entega do produto em sua residência.
+          </h1>
 
-        <h1 style={{ fontSize: "1.5rem" }}>
-          Oferecemos recomendações com base nos estilos musicais escolhidos
-          durante o cadastro. Fique a vontade e explore nosso site em busca dos
-          CDs ideais.
-        </h1>
+          <h1 style={{ fontSize: "1.5rem" }}>
+            Oferecemos recomendações com base nos estilos musicais escolhidos
+            durante o cadastro. Fique a vontade e explore nosso site em busca
+            dos CDs ideais.
+          </h1>
 
-        <h1 style={{ fontSize: "1.5rem" }}>
-            Nossa maior preocupação é oferecer uma experiência inesquecível para nossos usuários e parceiros.
-        </h1>
-      </div>
+          <h1 style={{ fontSize: "1.5rem" }}>
+            Nossa maior preocupação é oferecer uma experiência inesquecível para
+            nossos usuários e parceiros.
+          </h1>
+        </div>
+        <div className="QSButton">
+          <Button
+            style={{
+              backgroundColor: "#1A1423",
+              borderRadius: "10px",
+              color: "white",
+              Width: "100%",
+              height: "10vh",
+              fontWeight: "700",
+              fontSize: "2.5vh",
+              boxShadow: "2px 4px 5px 0px rgba(0,70,67,0.6)",
+            }}
+            onClick={() => {
+              history.push("Explorar");
+            }}
+          >
+            Explore Nossos Produtos!
+          </Button>
+        </div>
       </div>
       <div className="footer">
         <Rodape />
